33f525bec57ce38dfb2ca136c12b6a3b17b6d167
Hector <144190795+HectorOrea@users.noreply.github.com>
2026-07-08 10:16:23 -0400

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.py
@@ -0,0 +1,47 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        # go through our set
+        # do set membership to see how high up and down we can go
+        # remove these members and repeat until the list is empty
+
+
+    # Time: O(n) + O(n) + O(n)
+    # Space: O(n)
+        import random 
+
+        ns = set(nums)
+        max_len = 0
+        cur_len = 0
+
+        def check_up(x) -> int:
+            res = 0
+            while (x + 1) in ns:
+                res += 1
+                x += 1
+            return res, x
+        
+        def check_down(x) -> int:
+            res = 0
+            while (x - 1) in ns:
+                res +=1 
+                x -= 1
+            return res, x
+                
+        l = len(ns)
+        while l > 0:
+            n = next(iter(ns))
+
+            # remove contiguous seq found
+            # update max_len id necessary
+            l = len(ns)
+            a, A = check_down(n)
+            b, B = check_up(n)
+            cur_len = 1 + a + b
+            if cur_len > max_len:
+                max_len = cur_len
+            for m in range(A, B + 1):
+                ns.remove(m)
+            l = len(ns)
+        return max_len
+
+        
