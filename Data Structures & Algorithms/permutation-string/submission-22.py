fae5b8752d75dc59d83e41a0034c10d976d074b2
Hector <144190795+HectorOrea@users.noreply.github.com>
2026-07-09 13:05:05 -0400

    Add: permutation-string - submission-22

---
+++ Data Structures & Algorithms/permutation-string/submission-22.py
@@ -0,0 +1,54 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+
+        # initialize matches for s1 and initial window. Return True if it's a solution
+        s1count, win_count = [0] * 26, [0] * 26
+        for i in range(len(s1)):
+            s1count[ord(s1[i]) - ord('a')] += 1
+            win_count[ord(s2[i]) - ord('a')] += 1
+
+        matches = 0
+        for i in range(26):
+            if s1count[i] == win_count[i]:
+                matches += 1
+        
+        if matches == 26:
+            return True
+
+        for r in range(len(s1), len(s2)): # loop over 'next addition' to our window. The window at the end of this contains s2[r]
+            
+            l_remove = r - len(s1)
+            remove_idx = ord(s2[l_remove]) - ord('a')
+            add_idx = ord(s2[r]) - ord('a')
+
+            affected_idx = {remove_idx, add_idx}
+
+            matches_before = 0
+            for i in affected_idx:
+                if win_count[i] == s1count[i]:
+                    matches_before += 1
+                    
+            # update state for new window
+            win_count[remove_idx] -= 1
+            win_count[add_idx] += 1
+
+            matches_after = 0
+            for i in affected_idx:
+                if win_count[i] == s1count[i]:
+                    matches_after += 1
+            
+            # update matches
+            delta = matches_after - matches_before
+            matches += delta
+
+            # check if solution
+            if matches == 26:
+                return True
+        return False
+        
+        
+
+
+        
