493f4cc552b8ab65cb6b32caf2c2e5a15f866f49
Hector <144190795+HectorOrea@users.noreply.github.com>
2026-07-09 12:19:23 -0400

    Add: permutation-string - submission-11

---
+++ Data Structures & Algorithms/permutation-string/submission-11.py
@@ -0,0 +1,33 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+
+        target_freq_map = {chr(ord('a') + i): 0 for i in range(26)}
+        for c in s1:
+            target_freq_map[c] = target_freq_map.get(c, 0) + 1
+
+        window_freq_map = {chr(ord('a') + i): 0 for i in range(26)}        
+        for c in s2[:len(s1)]:
+            window_freq_map[c] = window_freq_map.get(c, 0) + 1
+
+
+        l, r = 0, len(s1) - 1
+        while (r < len(s2)):
+
+            # check if it's a valid solution
+            if target_freq_map == window_freq_map:
+                return True
+
+            # shift window
+            l += 1
+            r += 1
+
+            # update state
+            window_freq_map[s2[l-1]] = window_freq_map.get(s2[l-1], 0) - 1
+            if (r < len(s2)): # be careful to not do an index error
+                 window_freq_map[s2[r]] = window_freq_map.get(s2[r], 0) + 1      
+        return False
+
+
+        
